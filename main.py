bff6d2db95363a71bf0534cf6534e14fe2439cd1
Udit26085 <bhatiaudit55@gmail.com>
2026-02-02 00:46:45 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastmcp import FastMCP
+from fastmcp import FastMCP
+from langchain_tavily import TavilySearch
+from dotenv import load_dotenv
+load_dotenv()
+
+mcp = FastMCP(name='MediSundhan')
+
+@mcp.tool()
+async def search(query: str) -> str:
+    '''Use this tool to seach the internet
+        Args: 
+            query: The search query string 
+        
+        Returns 
+            Search results asa formatted string
+    '''
+    tool = TavilySearch(
+        search_depth='advanced', 
+        max_results=1,
+        topic='general'
+    )
+    try: 
+        results = tool.invoke(input=query)
+        return results['results'][0]
+    
+    except Exception as e:
+        f"Error performing search: {str(e)}"
+
+    
+# To conver this into a remote server
+if __name__ == "__main__":
+    mcp.run(transport='http', host='0.0.0.0', port=8000)
